2cfb483a72a88494b5af3f34e05389e3163e5d95
MainUddin1223 <2021md.main@gmail.com>
2022-03-24 09:31:36 +0600

    third

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.css'
 const Cart = ({ cart }) => {
+    let total = 0;
+    for (const product of cart) {
+        total = total + product.price
+    }
+    const tax = total * 0.1;
+    const fixedTax = tax.toFixed(2)
+    let shipping = 0;
+    for (const product of cart) {
+        shipping = shipping + product.shipping
+    }
+    const grandTotal = total + shipping + tax;
     return (
         <div className='cart-div'>
-            <h1>{cart.length}</h1>
             <div className='item-summary'>
                 <p className="cart-heading">Order Summary</p>
-                <p>Selected Items:</p>
-                <p>Total Price: $</p>
-                <p>Total Shipping Charge:</p>
-                <p>Tax:$ </p>
+                <p>Selected Items:{cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge:{shipping}</p>
+                <p>Tax:$ {fixedTax}</p>
             </div>
-            <h6>Grand Total: $</h6>
+            <h6>Grand Total: $ {grandTotal}</h6>
         </div>
     );
 };
